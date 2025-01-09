63bc982d61cdff1c3cf1b3047ed11603508498f0
Taihlor Elliott <taihlor.elliott@gmail.com>
2025-01-09 11:09:19 -0500

    locations postman functions

--- locations/locations_api/views.py
+++ locations/locations_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .serializers import LocationSerializer
+from .models import Location
+
+class LocationList(generics.ListCreateAPIView):
+    queryset = Location.objects.all().order_by('id') 
+    serializer_class = LocationSerializer 
+
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Location.objects.all().order_by('id')
+    serializer_class = LocationSerializer
